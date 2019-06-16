8cb31c1513cef3253303252af9db66c7b0b6c530
Uwy <uwy@live.fr>
2019-06-17 00:48:08 +0200

    added progress bar to player

--- assets/js/player.js
+++ assets/js/player.js
@@ -29,7 +29,7 @@ ready(function () {
 
     // Taken from https://codepen.io/nfj525/pen/rVBaab
     // Setup visualizer
-    function visualizer() {
+    (function () {
         let context = new AudioContext();
         let src = context.createMediaElementSource(audio);
         let analyser = context.createAnalyser();
@@ -44,39 +44,36 @@ ready(function () {
         analyser.fftSize = 256;
 
         let bufferLength = analyser.frequencyBinCount;
-        console.log(bufferLength);
 
         let dataArray = new Uint8Array(bufferLength);
         let outputWidth = canvas.width;
         let outputHeight = canvas.height;
 
         let barWidth = (outputWidth / bufferLength) * 2.5;
+        // Frequencies ranges from 0 to 255
         var barHeightRatio = outputHeight / 255;
-        
+
 
         function renderFrame() {
-
             requestAnimationFrame(renderFrame);
 
             let x = 0;
 
             analyser.getByteFrequencyData(dataArray);
-            
+
             ctx.clearRect(0, 0, outputWidth, outputHeight);
 
-            for (let i = 0; i < bufferLength; i++) {
-                let barHeight = dataArray[i] * barHeightRatio;
-                console.log()
+            dataArray.forEach(frequencyData => {
+                let barHeight = frequencyData * barHeightRatio;
 
                 ctx.fillStyle = '#4B483F';
                 ctx.fillRect(x, outputHeight - barHeight, barWidth, barHeight);
 
                 x += barWidth + 1;
-            }
+            });
         }
         renderFrame();
-
-    };
+    })();
 
     // Setup buttons
     (function () {
@@ -86,37 +83,68 @@ ready(function () {
 
         let isPlaying = false;
 
-        audio.onplaying = function () {
+        audio.addEventListener('playing', function () {
             isPlaying = true;
             addClass(playPause, 'playing');
-            visualizer();
-        }
+        });
 
-        audio.onpause = function () {
+        audio.addEventListener('pause', function () {
             isPlaying = false;
             removeClass(playPause, 'playing');
-        }
+        });
 
-        volRange.onchange = function () {
+        volRange.addEventListener('onchange', function () {
             audio.volume = volRange.value;
-            audio.muted = audio.volume === 0;
-        }
+        });
 
-        playPause.onclick = function () {
+        playPause.addEventListener('click', function () {
             if (isPlaying) {
                 audio.pause();
             } else {
                 audio.play();
             }
-        };
+        });
 
-        muteButton.onclick = function () {
+        muteButton.addEventListener('click', function () {
             audio.muted = !audio.muted;
             if (audio.muted) {
                 addClass(muteButton, 'muted');
             } else {
                 removeClass(muteButton, 'muted');
             }
+        });
+    })();
+
+    // Setup progress bar
+    (function () {
+        let progress = document.getElementById('progress');
+
+        // Taken from https://codepen.io/mdf/pen/ZWbvBv
+        let timer;
+        let percent = 0;
+
+        audio.addEventListener("playing", function (_event) {
+            let duration = _event.target.duration;
+            advance(duration, audio);
+        });
+
+        audio.addEventListener("pause", function (_event) {
+            clearTimeout(timer);
+        });
+
+        let advance = function (duration, element) {
+            let progress = document.getElementById("progress");
+            increment = 10 / duration
+            percent = Math.min(increment * element.currentTime * 10, 100);
+            progress.style.width = percent + '%'
+            startTimer(duration, element);
+        }
+
+        let startTimer = function (duration, element) {
+            if (percent < 100) {
+                timer = setTimeout(function () { advance(duration, element) }, 100);
+            }
         }
     })();
+
 });
